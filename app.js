e300f2bf480055f44f3eedf779698ce83107fe0e
bonnichiwa <bonnie.pham@ryerson.ca>
2015-10-02 11:37:40 -0400

    form can now render with image as ng transclude, showed 2 diff forms with diff images to demonstrate

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ app.directive('optIn', function() {
   return {
     restrict: 'E',
     transclude: true,
-    templateUrl: 'opt-in.html',
-
+    template: '<div class="main">' +
+              '<div ng-transclude></div>' +
+              '<form name="acme-form">' + 
+              '<input type="text" placeholder="First name"><br>' + 
+              '<input type="text" placeholder="Last name"><br>' +
+              '<input type="email" placeholder="E-mail"><br>' + 
+              '<input type="submit" value="Give us yr infoz!">' + 
+              '</form>' +
+              '</div>',
     replace: true
   }
 
